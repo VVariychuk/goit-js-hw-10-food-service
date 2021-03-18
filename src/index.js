1b71896b15253540ef26f86b1566358510c2afde
VVariychuk <71844197+VVariychuk@users.noreply.github.com>
2021-03-18 23:18:29 +0200

    create markup

--- src/index.js
+++ src/index.js
@@ -1,1 +1,23 @@
+import menuTemplate from './templates/food-cards.hbs';
+import foodCards from './menu.json';
 import './styles.css';
+
+
+const refs = {
+    menu: document.querySelector('.js-menu'),
+    bodyRef: document.querySelector('body'),
+    themeChek: document.getElementById('theme-switch-toggle')
+}
+
+const Theme = {
+  LIGHT: 'light-theme',
+  DARK: 'dark-theme',
+};
+
+function createMenu(foodCards) {
+    return menuTemplate(foodCards)
+}
+ 
+const createMenuMarkup = createMenu(foodCards)
+refs.menu.insertAdjacentHTML('beforeend', createMenuMarkup)
+
