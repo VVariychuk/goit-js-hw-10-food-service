ac0d166b20ea5cbedc6975aac3845a4aafa00e84
VVariychuk <71844197+VVariychuk@users.noreply.github.com>
2021-03-19 21:23:55 +0200

    добавляет работу с localStorage

--- src/index.js
+++ src/index.js
@@ -4,20 +4,56 @@ import './styles.css';
 
 
 const refs = {
-    menu: document.querySelector('.js-menu'),
-    bodyRef: document.querySelector('body'),
-    themeChek: document.getElementById('theme-switch-toggle')
-}
+  menu: document.querySelector('.js-menu'),
+  body: document.querySelector('body'),  
+  Checkbox: document.querySelector('#theme-switch-toggle'),
+};
 
 const Theme = {
   LIGHT: 'light-theme',
   DARK: 'dark-theme',
 };
 
-function createMenu(foodCards) {
-    return menuTemplate(foodCards)
-}
- 
-const createMenuMarkup = createMenu(foodCards)
-refs.menu.insertAdjacentHTML('beforeend', createMenuMarkup)
+const keyTheme = 'Theme';
 
+setTheme();
+
+let currentTheme;
+let newTheme;
+setStatusOfThemes();
+
+refs.Checkbox.addEventListener('change', changeTheme);
+
+const menuItemsMarkup = createMenuItems(foodCards);
+
+refs.menu.insertAdjacentHTML('beforeend', menuItemsMarkup);
+
+function changeTheme() {
+  refs.body.classList.remove(currentTheme);
+  refs.body.classList.add(newTheme);
+  localStorage.setItem(keyTheme, newTheme);
+
+  setStatusOfThemes();
+}
+
+function setTheme() {
+  const clientTheme = localStorage.getItem(keyTheme);
+
+  refs.body.classList.add(clientTheme);
+
+  if (clientTheme === Theme.DARK) {
+    refs.Checkbox.checked = true;
+  } else {
+    refs.body.classList.add(Theme.LIGHT);
+    localStorage.setItem(keyTheme, Theme.LIGHT);
+  }
+}
+
+function setStatusOfThemes() {
+  currentTheme = localStorage.getItem(keyTheme);
+  newTheme = currentTheme !== Theme.LIGHT ? Theme.LIGHT : Theme.DARK;
+}
+
+function createMenuItems(foodCards) {
+  return menuTemplate(foodCards);
+}
